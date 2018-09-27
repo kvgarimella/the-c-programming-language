6fe4234f438a4725516b981f570a2eeb9c42d327
Karthik Garimella <kvgarimella@gmail.com>
2018-09-27 09:40:16 -0500

    update e1-13 with vertical and horizontal histograms

--- chapter_1/exercises/E1-13.c
+++ chapter_1/exercises/E1-13.c
@@ -1,33 +1,66 @@
 #include <stdio.h>
 
+#define INWORD 1
+#define LONGESTWORD 15
+
 int main()
 {
-  int curr_length, total, array_size, i, c;
-  curr_length = total = 0;
-  array_size = 15;
+  int curr_length, total, i, j, c, curr_state, max;
+  curr_length = total = curr_state = max = 0;
 
-
-  int length_words[array_size];
-
-  for(i = 0; i < array_size; ++i) {
+  // initialize array: Longest word can be length 15.
+  int length_words[LONGESTWORD];
+  for(i = 0; i < LONGESTWORD; ++i) {
     length_words[i] = 0;
   }
 
   while ((c = getchar()) != EOF) {
+    if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z')) {
+      if (curr_state != INWORD) {
+        curr_state = INWORD;
+        curr_length = 1;
+      }
+      else {curr_length += 1;}
+    }
     if (c == '\n' || c == '\t' || c == ' ') {
-      ++length_words[curr_length];
-      if (curr_length > 0) {
+      if (curr_state == INWORD) {
+        ++length_words[curr_length];
         ++ total;
+        curr_state = 0;
       }
       curr_length = 0;
-
-    } else {
-      curr_length += 1;
     }
   }
-  printf("\nLength of words.\n");
-  for(i = 0; i < array_size; ++i) {
-    printf("%d: %d\n", i, length_words[i]);
+  printf("\nEOF found; now producing historgrams..\n\n");
+
+  printf("Horizontal Histogram:\n");
+  for(i = 0; i < LONGESTWORD; ++i) {
+    printf("%2d:", i);
+    for(j = 0; j < length_words[i]; ++j) {
+      putchar('#');
+    }
+    putchar('\n');
   }
+
+  printf("\nVertical Histogram:\n");
+  for(i = 0; i < LONGESTWORD; ++i) {
+    if (length_words[i] > max) {
+      max = length_words[i];
+    }
+  }
+  for(i = max - 1; i >= 0; --i) {
+    for(j = 0; j < LONGESTWORD; ++j) {
+      if (length_words[j] > i) {
+        printf(" #  ");
+      } else {
+        printf("    ");
+      }
+    }
+    putchar('\n');
+  }
+  for(i = 0; i < LONGESTWORD; ++i) {
+    printf("%2d  ", i);
+  }
+  putchar('\n');
 
 }
