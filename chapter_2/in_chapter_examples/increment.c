5a8808f95584aff8e04c6c99a3d42eaa8bc37482
Karthik Garimella <kvgarimella@gmail.com>
2019-01-01 19:39:29 -0500

    add example files

---
+++ chapter_2/in_chapter_examples/increment.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+  int x;
+
+  int n = 5;
+
+  x = n++;
+  printf("x = n++; -> x = %d, n = %d\n", x, n);
+
+  n = 5;
+
+  x = ++n;
+  printf("x = ++n; -> x = %d, n = %d\n", x, n);
+
+  return 0;
+}
---
+++ chapter_2/in_chapter_examples/squeeze.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void squeeze(char s[], int c);
+
+int main()
+{
+  char s[] = "abcdefg\0";
+  int c = 'c';
+
+  squeeze(s, c);
+
+  printf("%s\n", s);
+}
+
+void squeeze(char s[], int c)
+{
+  int i, j;
+  for (i = j = 0; s[i] != '\0'; i++)
+  {
+    if (s[i] != c)
+    {
+      s[j++] = s[i];
+    }
+  }
+  s[j] = '\0';
+}
