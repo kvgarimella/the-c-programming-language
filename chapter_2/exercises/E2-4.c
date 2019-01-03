d5d812bf20bb65d41a6788f405fd8fdd3ce2a6f3
Karthik Garimella <kvgarimella@gmail.com>
2019-01-03 15:29:14 -0500

    add exercise E2-4: delete characters from first string that match any character in a second string

---
+++ chapter_2/exercises/E2-4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+void squeeze_alt(char s1[], char s2[]);
+
+int main()
+{
+  char s1[] = "hello\0";
+  char s2[] = "hi there\0";
+  squeeze_alt(s1, s2);
+  printf("%s\n", s1);
+
+}
+
+void squeeze_alt(char s1[], char s2[])
+{
+  int i, j, k, c1, c2;
+
+  i = j = 0;
+  while ((c1 = s1[i]) != '\0')
+  {
+    for (k = 0; (c2 = s2[k]) != '\0'; k++)
+    {
+      if (c2 == c1)
+      {
+        break;
+      }
+    }
+    if (c2 == '\0')
+    {
+      s1[j++] = s1[i];
+    }
+    ++i;
+  }
+  s1[j] = '\0';
+}
