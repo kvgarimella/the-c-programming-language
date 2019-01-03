d34c8877d0baa31cd3ddb1d889186137f7f8c626
Karthik Garimella <kvgarimella@gmail.com>
2019-01-03 15:39:22 -0500

    update E2-4

--- chapter_2/exercises/E2-4.c
+++ chapter_2/exercises/E2-4.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/*
+deletes any character in s1 that matches nay character
+in s2.
+*/
+
 void squeeze_alt(char s1[], char s2[]);
 
 int main()
@@ -8,6 +13,7 @@ int main()
   char s2[] = "hi there\0";
   squeeze_alt(s1, s2);
   printf("%s\n", s1);
+  return 0;
 
 }
 
