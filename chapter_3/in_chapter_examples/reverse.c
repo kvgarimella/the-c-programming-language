b73e36a2b1740c77ee4c8ac2d5fbc5aacb30838f
Karthik Garimella <kvgarimella@gmail.com>
2019-01-06 15:27:43 -0600

    add in chapter example of in place reverse

---
+++ chapter_3/in_chapter_examples/reverse.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+void reverse (char[]);
+
+int main()
+{
+  char s[] = "Hello how are you doing?\0";
+  reverse(s);
+  printf("%s\n", s);
+  return 0;
+}
+
+void reverse(char s[])
+{
+  int c, i, j;
+
+  for (i = 0, j = strlen(s) - 1; i < j; i++,j--)
+  {
+    c = s[i];
+    s[i] = s[j];
+    s[j] = c;
+  }
+}
