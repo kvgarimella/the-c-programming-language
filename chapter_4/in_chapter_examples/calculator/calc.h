14c95364d8aaf2a7b071edb5fa561f3234de061c
Karthik Garimella <kvgarimella@gmail.com>
2019-01-08 15:33:50 -0600

    add rpn calculator as several source files

---
+++ chapter_4/in_chapter_examples/calculator/calc.h
@@ -0,0 +1,6 @@
+#define NUMBER '0'
+int getch(void);
+void ungetch(int);
+int getop(char []);
+void push(double);
+double pop(void);
---
+++ chapter_4/in_chapter_examples/calculator/stack.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include "calc.h"
+#define MAXVAL 100
+
+double val[MAXVAL];
+int sp = 0;
+
+void push(double f)
+{
+  if (sp < MAXVAL)
+  {
+    val[sp++] = f;
+  }
+  else
+  {
+    printf("error: stack full, can't push %g\n", f);
+  }
+}
+
+double pop(void)
+{
+  if (sp > 0)
+  {
+    return val[--sp];
+  }
+  else
+  {
+    printf("error: stack empty.\n");
+    return 0.0;
+  }
+}
