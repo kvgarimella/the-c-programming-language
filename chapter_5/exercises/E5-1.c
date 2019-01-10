d3124f9817508760c56e4fa9957e9f78fd1d8d34
Karthik Garimella <kvgarimella@gmail.com>
2019-01-09 18:22:28 -0600

    add e5-1: i dont push back.. just wait for the first nonblank

---
+++ chapter_5/exercises/E5-1.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <ctype.h>
+#define SIZE 5
+#define BUFSIZE 100
+
+int buf[BUFSIZE];
+int bufp = 0;
+
+int getch(void);
+void ungetch(int);
+int getint(int *);
+
+
+int main()
+{
+  int n, array[SIZE], i;
+  for (n = 0; n < SIZE && getint(&array[n]) != EOF; n++) {}
+
+  for (i = 0; i < SIZE; i++)
+  {
+    printf("%d\n", array[i]);
+  }
+}
+
+int getint(int *pn)
+{
+  int c, sign;
+
+  while (isspace(c = getch())) {}
+
+  if (!isdigit(c) && c != EOF && c != '+' && c != '-')
+  {
+    ungetch(c);
+    return 0;
+  }
+  sign = (c == '-') ? -1 : 1;
+  if (c == '+' || c == '-')
+  {
+    while (isblank(c = getch())) {}
+  }
+
+  for (*pn = 0; isdigit(c); c = getch())
+  {
+    *pn = 10 * *pn + (c - '0');
+  }
+
+  *pn *= sign;
+
+  if (c != EOF)
+  {
+    ungetch(c);
+  }
+  return c;
+}
+
+int getch(void)
+{
+  return (bufp > 0) ? buf[--bufp] : getchar();
+}
+
+void ungetch(int c)
+{
+  if (bufp >= BUFSIZE)
+  {
+    printf("ungetch: too many characters\n");
+  }
+  else
+  {
+    buf[bufp++] = c;
+  }
+}
