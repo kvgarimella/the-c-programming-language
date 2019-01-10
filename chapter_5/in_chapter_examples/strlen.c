f3d21dcf3619c30a805deb78d2fe85a07b246c2a
Karthik Garimella <kvgarimella@gmail.com>
2019-01-09 18:45:17 -0600

    pointer version of strlen..

---
+++ chapter_5/in_chapter_examples/strlen.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int strlen_p(char *);
+
+int main()
+{
+  char s[] = "hello, there\0";
+  printf("%d\n", strlen_p(s));
+  printf("%d\n", strlen_p(&s[0]));
+  printf("%d\n", strlen_p("hello, world!"));
+  return 0;
+}
+
+int strlen_p(char *s)
+{
+  int n;
+  for (n = 0; *s != '\0'; s++)
+  {
+    n++;
+  }
+  return n;
+}
